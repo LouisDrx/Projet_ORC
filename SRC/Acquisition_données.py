8b17bb38580feebc9f4b08c35f40e7f7ca6a9ad1
Erwan Sauzay <erwan.sauzay@uha.fr>
2024-06-01 16:19:57 +0200

    commenter le tx

--- SRC/Acquisition_données.py
+++ SRC/Acquisition_données.py
@@ -1,3 +1,5 @@
+
+# Import 
 import json
 import jsonpath_ng as jp #actuellement pb avec le path
 import numpy as np
@@ -6,24 +8,24 @@ with open("trajectory.jason", "r") as f: #on ouvre le fichier json et on place l
 
 t=0.5
 query1=jp.parse("pose.position")
-coord_x=[]
+coord_x=[] 
 coord_y=[]
-vitesse_l=[]
-vitesse_a=[]
+vitesse_l=[] #Vitesse linéaire
+vitesse_a=[] #Vitesse angulaire 
 
+# Extraction des données
 for i in range(800):#on selectionne uniquement x et y 
-    coord_x.append(0)
-    coord_y.append(0)
-    coord_x[i]=data[i]["pose"]['position']['x']
-    coord_y[i]=data[i]["pose"]['position']['y']
+    coord_x.append(data[i]["pose"]['position']['x'])
+    coord_y.append(data[i]["pose"]['position']['y'])
 
+# Calcul des vitesses linéiare et angulaire
 for i in range(799):
-    vitesse_l.append()
-    vitesse_a.append()
-    vitesse_l[i]=(((coord_x[i+1]-coord_x[i])/t)+((coord_y[i+1]-coord_y[i])/t))/2#calcul de la vitesse avec les coordonnées récupérées
+    vitesse_l.append((((coord_x[i+1]-coord_x[i])/t)+((coord_y[i+1]-coord_y[i])/t))/2)
+    
+
     if (coord_x[i+1]-coord_x[i])==0:
-        vitesse_a[i]=0
+        vitesse_a.append(0)
     else:
-        vitesse_a[i]=np.arctan(((coord_y[i+1]-coord_y[i])/(coord_x[i+1]-cord_x[i])))/t
+        vitesse_a.append(np.arctan(((coord_y[i+1]-coord_y[i])/(coord_x[i+1]-coord_x[i])))/t)
 
 print(vitesse_l)
