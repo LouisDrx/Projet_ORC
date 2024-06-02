3c8333e1e2e78a1977b62fd7ad26b5a7bdefe04d
Erwan Sauzay <erwan.sauzay@uha.fr>
2024-06-02 21:25:56 +0200

    test_1

--- SRC/test_GUI_finished.py
+++ SRC/test_GUI_finished.py
@@ -1,12 +1,18 @@
 import pytest
 import GUI_finished
 
-@pytest.mark.parametrize("input",["A","B","C"])
+@pytest.mark.parametrize("input",["192.168.0.01","192.168.100.21"])
 def test_AdresseIP(input:chr):
-    a = "3"
-    assert type(a) == str
+    
+    assert len(input)>=11 , "L'IP attendu fait au moins 12 characteres"
+    assert len(input)<=15 , "L'IP attendu fait au plus 15 characteres"
+    assert int(input[0:3]) == 192 , "IP =192.168.X.X"
+    assert int(input[4:7]) == 168 , "IP =192.168.X.X"
 
-@pytest.mark.parametrize("input",["A","B","C"])
-def Prt_valide(input:int):
-    a = "3"
-    assert type(a) == str
+
+@pytest.mark.parametrize("input",["4","30"])
+def Port_valide(input:chr):
+
+    assert int(type(input)) >0 , "port >0"
+
+
